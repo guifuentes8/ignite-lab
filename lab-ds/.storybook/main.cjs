f549c454981eada74ac75c22f2bc97c9be8f8534
guilherme.fuentes <guilherme.fuentes@accurate.com.br>
2022-10-14 11:33:40 -0300

    upload cd/ci storybook

--- lab-ds/.storybook/main.cjs
+++ lab-ds/.storybook/main.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/lab-ds/";
+      config.base = "/ignite-lab/";
     }
     return config;
   },
